e5b9ca3e3201aac51d71791aaffdd6f6c9285214
Mahmoud Atef <Mahmoudbakar2002@github.com>
2024-08-03 19:06:45 +0300

    db update

--- src/db/config.js
+++ src/db/config.js
@@ -10,7 +10,11 @@ const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 const connect = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(DB_CONNECTION_STRING);
+    await mongoose.connect(DB_CONNECTION_STRING, {
+      connectTimeoutMS: 30000,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
     console.log("connected to db successful.");
   } catch (err) {
     console.log("error on db connection", err);
